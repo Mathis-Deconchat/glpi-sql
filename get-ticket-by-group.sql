fd0f8afbcb4e10e0eff95fcf9430eb43bd150f89
mathis.deconchat <mathis.deconchat@gmail.com>
2021-03-17 20:24:17 +0100

    new request

---
+++ get-ticket-by-group.sql
@@ -0,0 +1,8 @@
+SELECT count(gt.id) 
+FROM glpi_tickets
+JOIN glpi_groups_tickets AS ggt ON gt.id = ggt.tickets_id
+JOIN glpi_groups AS gg ON ggt.groups_id = gg.id
+WHERE gt.status NOT IN (5,6)
+AND gt.is_deleted = 0 
+AND ggt.`type` = 2
+GROUP BY gg.name
